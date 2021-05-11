bc0aabdcb92316ce8f507d68e0b60a5d62e71404
LidraFoxfire <vanderwath.ulrich121@gmail.com>
2021-05-11 17:12:27 +0200

    Space between the Fields

--- lib/screens/authenticate/login.dart
+++ lib/screens/authenticate/login.dart
@@ -118,6 +118,7 @@ class _LoginState extends State<Login> {
                                   },
                                 ),
                               ),
+                              SizedBox(height: 10),
                               Container(
                                 decoration: BoxDecoration(
                                   color: Colors.grey[600].withOpacity(0.5),
