7d58bca34aeb855d3fca8ec72e8708e5b50aa8d0
LidraFoxfire <vanderwath.ulrich121@gmail.com>
2021-05-11 19:46:10 +0200

    Forgot

---
+++ lib/Widgets/forgetBackImg.dart
@@ -0,0 +1,32 @@
+import 'package:flutter/material.dart';
+
+class forgetImg extends StatelessWidget {
+  const forgetImg({
+    Key key,
+  }) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return ShaderMask(
+
+      shaderCallback: (bounds) =>  LinearGradient(
+        colors: [Colors.black, Colors.black12],
+        begin: Alignment.bottomCenter,
+        end: Alignment.center,
+      ).createShader(bounds),
+      blendMode: BlendMode.darken,
+      child: Container(
+        decoration: BoxDecoration(
+            image: DecorationImage(
+                image: AssetImage('pics/screen-1.jpg'),
+                fit: BoxFit.cover,
+                colorFilter: ColorFilter.mode(Colors.black45, BlendMode.darken)
+            )
+        ),
+      ),
+
+
+
+    );
+  }
+}
---
+++ lib/screens/authenticate/forgotPass.dart
@@ -0,0 +1,127 @@
+import 'package:flutter/material.dart';
+import 'package:pb_blueprotocal/services/auth.dart';
+import 'package:pb_blueprotocal/shared/constants.dart';
+import 'package:flutter/widgets.dart';
+import 'package:pb_blueprotocal/Widgets/forgetBackImg.dart';
+
+void main() {
+  runApp(MaterialApp(
+    home: log(),
+  ));
+}
+
+class log extends StatelessWidget {
+  const log({Key key}) : super(key: key);
+
+
+  @override
+  Widget build(BuildContext context) {
+    return Stack(
+      children: [
+        forgetImg(),
+        Scaffold(
+          backgroundColor: Colors.transparent,
+          body: SingleChildScrollView(
+            child: SafeArea(
+              child: Column(
+                children: [
+                  Column(
+                    children: [
+                      SizedBox(height: 0),
+                      Container(
+                        width: double.infinity,
+                        decoration: BoxDecoration(
+                            color: Colors.blue,
+                            borderRadius: BorderRadius.circular(16)
+
+                        ),
+
+                        child: Row(
+                          children: [
+                            Container(
+                              child: FlatButton(
+                                onPressed: (){},
+                                child: Padding(
+                                  padding: const EdgeInsets.symmetric(vertical: 16.0),
+                                  child: Text('Back', style: kbod,),
+                                ),
+                              ),
+                            )
+                          ],
+                        ),
+                      ),
+                    ],
+                  ),
+                  SizedBox(
+                    height: 300,),
+                  Container(
+                    padding: const EdgeInsets.symmetric(horizontal: 40),
+                    child: Form(
+                      child: Column(
+                        children:[ Column(
+                          crossAxisAlignment: CrossAxisAlignment.end,
+                          children: [
+                            Padding(
+                              padding: const EdgeInsets.symmetric(vertical: 12),
+                              child: Container(
+                                decoration: BoxDecoration(
+                                  color: Colors.grey[600].withOpacity(0.5),
+                                  borderRadius: BorderRadius.circular(16),
+
+                                ),
+                                child: TextFormField(
+                                  decoration: InputDecoration(
+                                    contentPadding: const EdgeInsets.symmetric(vertical: 20),
+                                    border: InputBorder.none,
+                                    hintText: 'Forgot Password',
+
+                                    prefixIcon: Padding(
+                                      padding: const EdgeInsets.symmetric(horizontal: 20),
+                                      child: Icon(Icons.login,
+                                        color: Colors.white,
+                                        size: 30,
+                                      ),
+                                    ),
+                                    hintStyle: kbod,
+                                  ),
+                                  style: kbod,
+
+                                ),
+                              ),
+                            ),
+                          ],
+                        ),
+                          Column(
+                            children: [
+                              SizedBox(height: 50),
+                              Container(
+                                width: double.infinity,
+                                decoration: BoxDecoration(
+                                    color: Colors.blue,
+                                    borderRadius: BorderRadius.circular(16)
+
+                                ),
+                                child: FlatButton(
+                                  onPressed: (){},
+                                  child: Padding(
+                                    padding: const EdgeInsets.symmetric(vertical: 16.0),
+                                    child: Text('Submit', style: kbod,),
+                                  ),
+                                ),
+                              ),
+                            ],
+                          )
+                        ],),
+                    ),
+                  ),
+                ],
+              ),
+            ),
+          )
+          ,
+        ),
+
+      ],
+    );
+  }
+}
