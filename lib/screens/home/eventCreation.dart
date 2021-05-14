a6e4f54adf998e887298445e5246d7e4f4e875e2
chadDowman <61121029+chadDowman@users.noreply.github.com>
2021-05-14 18:48:07 +0200

    MASSIVE UPDATE AUJ

--- lib/screens/home/eventCreation.dart
+++ lib/screens/home/eventCreation.dart
@@ -1,5 +1,12 @@
 import 'package:flutter/material.dart';
+import 'package:fluttertoast/fluttertoast.dart';
+import 'package:pb_blueprotocal/models/event.dart';
+import 'package:pb_blueprotocal/models/user.dart';
+import 'package:pb_blueprotocal/services/database.dart';
+import 'package:pb_blueprotocal/services/eventDatabase.dart';
 import 'package:pb_blueprotocal/shared/constants.dart';
+import 'package:pb_blueprotocal/shared/loading.dart';
+import 'package:provider/provider.dart';
 
 class EventCreation extends StatefulWidget {
   @override
@@ -7,100 +14,143 @@ class EventCreation extends StatefulWidget {
 }
 
 class _EventCreationState extends State<EventCreation> {
+
+  final _formKey = GlobalKey<FormState>();
+  String eventID;
+  String eventName;
+  String eventDescription;
+  String uid;
+
   @override
   Widget build(BuildContext context) {
-    return Stack(
-      children: [
-        Scaffold(
-          backgroundColor: Colors.grey[900],
-          body: SingleChildScrollView(
-            child: SafeArea(
-              child: Column(
-                children: [
-                  SizedBox(
-                    height: 50,
-                  ),
-                  Container(
-                    padding: const EdgeInsets.symmetric(horizontal: 40),
-                    child: Form(
-                      child: Column(
-                        crossAxisAlignment: CrossAxisAlignment.start,
-                        children: <Widget>[
-                          Divider(
-                            height: 60,
-                            color: Colors.grey[800],
-                          ),
-                          Text(
-                            'Enter the name of the event',
-                            style: TextStyle(
-                              color: Colors.white,
-                              letterSpacing: 2,
+    final user = Provider.of<User>(context);
+
+    return StreamProvider<Event>.value(
+      value: DatabaseService().eventData,
+      child: Stack(
+        children: [
+          Scaffold(
+            backgroundColor: Colors.grey[900],
+            body: SingleChildScrollView(
+              child: SafeArea(
+                child: Column(
+                  children: [
+                    SizedBox(
+                      height: 50,
+                    ),
+                    Container(
+                      padding:
+                      const EdgeInsets.symmetric(horizontal: 40),
+                      child: Form(
+                        key: _formKey,
+                        child: Column(
+                          crossAxisAlignment:
+                          CrossAxisAlignment.start,
+                          children: <Widget>[
+                            Divider(
+                              height: 60,
+                              color: Colors.grey[800],
                             ),
-                          ),
-                          SizedBox(height: 10),
-                          TextFormField(
-                            style: TextStyle(color: Colors.white),
-                            decoration: InputDecoration(
-                              contentPadding:
-                              const EdgeInsets.symmetric(vertical: 20),
-                              border: InputBorder.none,
-                              hintText: 'Event Name',
-                              prefixIcon: Icon(
-                                Icons.drive_file_rename_outline,
+                            Text(
+                              'Enter the name of the event',
+                              style: TextStyle(
                                 color: Colors.white,
+                                letterSpacing: 2,
                               ),
-                              hintStyle: kbod,
                             ),
-                          ),
-                          SizedBox(height: 10),
-                          Text(
-                            'Describe the Event that is taking place',
-                            style: TextStyle(
-                              color: Colors.white,
-                              letterSpacing: 2,
+                            SizedBox(height: 10),
+                            TextFormField(
+                              style: TextStyle(color: Colors.white),
+                              decoration: InputDecoration(
+                                contentPadding:
+                                const EdgeInsets.symmetric(
+                                    vertical: 20),
+                                border: InputBorder.none,
+                                hintText: 'Event Name',
+                                prefixIcon: Icon(
+                                  Icons.drive_file_rename_outline,
+                                  color: Colors.white,
+                                ),
+                                hintStyle: kbod,
+                              ),
+                              validator: (val) => val.isEmpty ? "Enter A Event Name!" : null,
+                              onChanged: (val) {
+                                setState(() {
+                                  eventName = val;
+                                });
+                              },
                             ),
-                          ),
-                          SizedBox(height: 10),
-                          TextFormField(
-                            style: TextStyle(color: Colors.white),
-                            decoration: InputDecoration(
-                              contentPadding:
-                              const EdgeInsets.symmetric(vertical: 20),
-                              border: InputBorder.none,
-                              hintText: 'Description',
-                              prefixIcon: Icon(
-                                Icons.event,
+                            SizedBox(height: 10),
+                            Text(
+                              'Describe the Event that is taking place',
+                              style: TextStyle(
                                 color: Colors.white,
+                                letterSpacing: 2,
                               ),
-                              hintStyle: kbod,
                             ),
-                          ),
-                          SizedBox(height: 30),
-                          Row(
-                            children: [
-                              ElevatedButton(
-                                onPressed: () {},
-                                child: Padding(
-                                  padding: const EdgeInsets.symmetric(
-                                      vertical: 16.0),
-                                  child: Text(
-                                    'Change',
-                                    style: kbod,
+                            SizedBox(height: 10),
+                            TextFormField(
+                              style: TextStyle(color: Colors.white),
+                              decoration: InputDecoration(
+                                contentPadding:
+                                const EdgeInsets.symmetric(
+                                    vertical: 20),
+                                border: InputBorder.none,
+                                hintText: 'Description',
+                                prefixIcon: Icon(
+                                  Icons.event,
+                                  color: Colors.white,
+                                ),
+                                hintStyle: kbod,
+                              ),
+                              validator: (val) => val.length < 10 ? "Enter A Event Description!" : null,
+                              onChanged: (val) {
+                                setState(() {
+                                  eventDescription = val;
+                                });
+                              },
+                            ),
+                            SizedBox(height: 30),
+                            Row(
+                              children: [
+                                ElevatedButton(
+                                  onPressed: () async{
+                                    if (_formKey.currentState.validate()) {
+                                      eventID = eventName;
+                                      uid = user.uid;
+                                      await changeEventDetails();
+                                    }
+                                  },
+                                  child: Padding(
+                                    padding:
+                                    const EdgeInsets.symmetric(
+                                        vertical: 16.0),
+                                    child: Text(
+                                      'Post Event',
+                                      style: kbod,
+                                    ),
                                   ),
                                 ),
-                              ),
-                            ],
-                          ),
-                        ],
+                              ],
+                            ),
+                          ],
+                        ),
                       ),
                     ),
-                  ),
-                ],
+                  ],
+                ),
               ),
             ),
           ),
-        ),
-      ],
+        ],
+      ),
     );
+  }
+
+  changeEventDetails() async {
+    print("eventName");
+    await DatabaseService(uid: eventName).updateEventData(uid, eventName, eventDescription);
+
+    Fluttertoast.showToast(msg: "User Event Successfully Updated");
   }
 }
